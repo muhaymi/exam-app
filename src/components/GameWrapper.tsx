7779c93981d09cd1e88d96e991941844898fde2b
muhaymi <muhayminurdin@gmail.com>
2025-09-24 22:38:13 +0700

    clean

--- src/components/GameWrapper.tsx
+++ src/components/GameWrapper.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, {  useRef  } from "react";
 import DashboardLayout from "../layouts/DashboardLayout";
 
 const GameWrapper: React.FC = () => {
